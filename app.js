59fa05f1cb3343e45d4788b2556f1cd27bbc964d
grace-123-tech <gcogeeva@gmail.com>
2024-09-15 02:40:35 +0100

    Update html

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
 
     for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://st2.depositphotos.com/1424188/12247/i/450/depositphotos_122471052-stock-photo-woman-pilates-ball-exercises-fitness.jpg")
         .then(response => response.json())
         .then(data => {
             image.src = data.message
-            image.width = 100;
-            image.height = 100;
+            image.width = 80;
+            image.height = 80;
          })
     }
 
 })
+
+s
